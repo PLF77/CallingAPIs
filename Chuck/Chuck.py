0787148457612ac0f98f4eae394dadd3cc0e81ee
PLF77 <60972152+PLF77@users.noreply.github.com>
2021-02-14 13:30:05 +0100

    api avec flask

--- Chuck/Chuck.py
+++ Chuck/Chuck.py
@@ -1,69 +1,72 @@
 import json
 import requests
+from flask import Flask, request
 
 
-def get_random(params):
-    params = params.split(' ')
-    if len(params) > 1:
-        raise ValueError('Syntax: chuck <category>')
-    category = params[0]
+app = Flask(__name__)
+
+@app.route("/random", methods=["GET"])
+def get_random():
+    category = request.args.get('cat','')
     if category == '':
         response = requests.get('https://api.chucknorris.io/jokes/random')
-        print(response.json()['value'])
-        return
+        return response.json()['value']
     elif category not in requests.get('https://api.chucknorris.io/jokes/categories').json():
         raise ValueError('Wrong category...')
     else:
         response = requests.get(f'https://api.chucknorris.io/jokes/random?category={category}')
-        print(response.json()['value'])
-        return
+        return response.json()['value']
 
-
+@app.route("/cat", methods=["GET"])
 def cat(params):
+    str = ''
     if params != '':
         raise ValueError('Syntax: cat')
     else:
         cat = requests.get('https://api.chucknorris.io/jokes/categories').json()
     for i in cat:
-        print(i)
+        str += i
+    return str
 
+app.run(host='0.0.0.0', threaded=True)
 
-def split_command(command):
-    """
-    split command into command word and command parameters
-    :param command: initial command
-    :return: command word and command parameters in list
-    """
-    command = command.strip()
-    tokens = command.split(' ', 1)
-    command_word = tokens[0].strip().lower()
-    command_params = tokens[1].strip() if len(tokens) == 2 else ''
-    return command_word, command_params
+# def split_command(command):
+#     """
+#     split command into command word and command parameters
+#     :param command: initial command
+#     :return: command word and command parameters in list
+#     """
+#     command = command.strip()
+#     tokens = command.split(' ', 1)
+#     command_word = tokens[0].strip().lower()
+#     command_params = tokens[1].strip() if len(tokens) == 2 else ''
+#     return command_word, command_params
+#
+# def options():
+#     print('>chuck')
+#     print('>chuck <category>')
+#     print('>cat')
+#
+#
+# def start_command_ui():
+#     command_dict = {'chuck': get_random, 'cat': cat}
+#     done = False
+#     options()
+#     while not done:
+#         command = input('command> ')
+#         try:
+#             # split into command_word and command_params
+#             cmd_word, cmd_params = split_command(command)
+#             # have separate functions for each command word
+#             if cmd_word in command_dict:
+#                 command_dict[cmd_word](cmd_params)
+#             elif cmd_word == 'exit':
+#                 done = True
+#             else:
+#                 print('bad command')
+#         except ValueError as ve:
+#             print(str(ve))
+#
+#
+# #start_command_ui()
 
-def options():
-    print('>chuck')
-    print('>chuck <category>')
-    print('>cat')
-
-
-def start_command_ui():
-    command_dict = {'chuck': get_random, 'cat': cat}
-    done = False
-    options()
-    while not done:
-        command = input('command> ')
-        try:
-            # split into command_word and command_params
-            cmd_word, cmd_params = split_command(command)
-            # have separate functions for each command word
-            if cmd_word in command_dict:
-                command_dict[cmd_word](cmd_params)
-            elif cmd_word == 'exit':
-                done = True
-            else:
-                print('bad command')
-        except ValueError as ve:
-            print(str(ve))
-
-
-start_command_ui()
